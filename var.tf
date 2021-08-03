023a2d037bc9a48588a13362594339e8984e6e09
Raj kumar Maurya <rajkumar25maurya@gmail.com>
2021-08-03 09:35:15 +0530

    Updated var.tf file

--- var.tf
+++ var.tf
@@ -1,8 +1,10 @@
 variable "region" {
+    default = "us-east-1"
     
     }
 
 variable "vpc_cidr" {
+    default = "10.0.0.0/16"
 
 }
 
@@ -16,13 +18,15 @@ variable "ami" {
 }
 
 variable "public_subnet_cidr" {
+    default = "10.0.0.0/24"
 
 }
 
 variable "private_subnet_cidr" {
+    default = "10.0.16.0/20"
     
 }
 
-##variable "availability_zone" {
-    
-#}
+variable "availability_zone" {
+    default = "us-east-1a"    
+}
