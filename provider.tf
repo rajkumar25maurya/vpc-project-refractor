9eaab746aeacddaaa9ab3cf5b4fde33f8f1a154f
Raj kumar Maurya <rajkumar25maurya@gmail.com>
2021-08-03 09:45:10 +0530

    Updated provide file

--- provider.tf
+++ provider.tf
@@ -1,3 +1,12 @@
-#provider "aws" {
-##    region = "${var.region}"
-#}
+provider "aws" {
+  region  = "us-east-1"
+}
+
+
+terraform {
+  backend "s3" {
+    bucket = "raj-s3"
+    key    = "tf.tfstate"
+    region = "us-east-1"
+    }
+}
